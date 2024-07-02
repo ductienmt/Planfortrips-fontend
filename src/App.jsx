aefef90199b910c93fb7f928edbe4118f4c67723
ductienmt <dinhtangductien26@gmail.com>
2024-07-02 16:04:41 +0700

    create folder header, footer, hotel, restuarant

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Header from "./client/header/header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
